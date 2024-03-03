d9f99efd6460c67ffd39d2b6e324c4c66d5e18d2
Jesimiel <jesimielbarbosa@gmail.com>
2024-03-03 15:45:24 -0300

    implementado metodo para deslogar usuario

--- app/src/main/java/com/example/projetofirebase/TelaPrincipal.java
+++ app/src/main/java/com/example/projetofirebase/TelaPrincipal.java
@@ -2,13 +2,44 @@ package com.example.projetofirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TelaPrincipal extends AppCompatActivity {
+
+    private TextView    textNomeUser, textEmailUser;
+    private Button      bt_deslogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+
+        IniciarComponentes();
+
+        bt_deslogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseAuth.getInstance().signOut(); //encerrando usuario
+                Intent intent   = new Intent(TelaPrincipal.this, FormLogin.class); // direcionando para tela de login
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
     }
+
+    private void IniciarComponentes(){
+        textNomeUser    = findViewById(R.id.textNomeUser);
+        textEmailUser   = findViewById(R.id.textEmailUser);
+        bt_deslogar     = findViewById(R.id.bbt_deslogar);
+    }
+
 }
